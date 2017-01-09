308c522f07b4f53b723fe8356f25a8cdd79bb29b
93814690 <93814690@qq.com>
2017-01-09 21:37:34 +0800

    v 0.2.0 添加挡板以及得分

--- 1-BouncingBall-F.c
+++ 1-BouncingBall-F.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "windows.h"
+#include "conio.h"
 
-int x,y;
+int ball_x,ball_y;
 int velocity_x,velocity_y;
 int high,width;
+int bat_y;
+int score;
 
 //清屏函数
 void Cls(int x,int y)
@@ -26,12 +29,14 @@ void HideCursor()
 // 数据初始化
 void startup()
 {
-    x = 5;
-    y = 10;
+    ball_x = 5;
+    ball_y = 10;
     velocity_x = 1;
     velocity_y = 1;
     width = 30;
     high = 20;
+    bat_y = 12;
+    score = 0;
 }
 
 // 显示画面
@@ -44,31 +49,57 @@ void show()
     {
     	for(j = 0;j < width;j++)
     	{
-    		if ((i == x)&&(j == y))
-    			printf("o");
+    		if ((i == ball_x)&&(j == ball_y))
+    			printf("O");
+            else if((i != high - 1)||((j < bat_y)||(j > bat_y + 7)))
+                printf(" ");
     		else
-    			printf(" ");
+    			printf("*");
     	}
     	printf("\n");
     }
-    Sleep(50);
+    printf("得分：%d\n", score);
+    Sleep(150);
 }
 
 // 与用户输入无关的更新
 void updateWithoutInput()
 {
-	x += velocity_x;
-	y += velocity_y;
-	if ((x == 0)||(x == high))
+	if (ball_x == high - 1)
+    {
+        if((ball_y >= bat_y)&&(ball_y <= bat_y + 7))
+        {
+            velocity_x = -velocity_x;
+            score++;
+        }
+        else
+        {
+            printf("游戏失败\n");
+            system("pause");
+            exit(0);
+        }
+    }
+    ball_x += velocity_x;
+    ball_y += velocity_y;
+    if (ball_x == 0)
 		velocity_x = -velocity_x;
-	if ((y == 0)||(y == width))
+	if ((ball_y == 0)||(ball_y == width))
 		velocity_y = -velocity_y;
-}
+}   
+
 
 // 与用户输入有关的更新
 void updateWithInput()
 {
-
+    char input;
+    if (kbhit())
+    {
+        input = getch();
+        if ((input == 'a')&&(bat_y > 0))
+            bat_y -= 2;
+        if ((input == 'd')&&(bat_y < width - 1))
+            bat_y += 2;
+    }
 }
 
 int main()
@@ -83,3 +114,5 @@ int main()
 
     return 0;
 }
+
+
